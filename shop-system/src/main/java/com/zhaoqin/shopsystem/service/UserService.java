5c1123c643dcd1c70114ed833f66ca56fad94166
zhaoqin123666 <18040027158@163.com>
2020-03-18 19:44:34 +0800

    springBoot项目启动把库存存进redis

--- shop-system/src/main/java/com/zhaoqin/shopsystem/service/UserService.java
+++ shop-system/src/main/java/com/zhaoqin/shopsystem/service/UserService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
  * @Author zhaoqin
  * @Date 2020/3/4
  */
-@Service
 public interface UserService {
 
     User isExist(String username, String password);

3ab442c9b3d5c98cdc552423cd25191d2891e4e6
zhaoqin123666 <18040027158@163.com>
2020-03-18 19:43:38 +0800

    springBoot项目启动把库存存进redis

--- shop-system/src/main/java/com/zhaoqin/shopsystem/controller/UserController.java
+++ shop-system/src/main/java/com/zhaoqin/shopsystem/controller/UserController.java
@@ -9,7 +9,6 @@ import com.zhaoqin.shopsystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;

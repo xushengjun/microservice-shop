408c7756e5d9b1cbf90278b1803dab3d5b1925f0
zhaoqin123666 <18040027158@163.com>
2020-03-22 17:23:05 +0800

    springBoot项目启动把库存存进redis

---
+++ shop-common/src/main/java/面试题/Demp.java
@@ -0,0 +1,15 @@
+package 面试题;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Demp {
+    public static void main(String[] args) {
+
+        Map map = new LinkedHashMap();
+        map.put("1","2");
+        map.put("2","2");
+        System.out.println(map);
+    }
+}
